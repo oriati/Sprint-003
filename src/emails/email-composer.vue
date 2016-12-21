82eb8c8e81e0339baff37287c60be7395d1d1459
Ori Atias <oriati@gmail.com>
2016-12-21 17:13:08 +0200

    email 1st

---
+++ src/emails/email-composer.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <form>
+            <div class="form-group">
+                <label for="adress">Send To</label>
+                <input type="email" class="form-control" id="adress" aria-describedby="email-adress" placeholder="Adress">
+            </div>
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <input type="text" class="form-control" id="subject" aria-describedby="email-subject" placeholder="">
+            </div>
+            <div class="form-group">
+                <label for="body">Your Massage</label>
+                <textarea class="form-control" id="body" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="Attachments">Attach A File</label>
+                <input type="file" class="form-control-file" id="Attachments" aria-describedby="email-attach">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </section>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- src/pages/emails.vue
+++ src/pages/emails.vue
@@ -2,17 +2,24 @@
   <section>
 
     <h1>EMAILS</h1>
+    <div>
+      <email-center></email-center>
+    </div>
 
   </section>
 </template>
 
 <script>
+import emailCenter from '../emails/email-center.vue'
 export default {
   name: 'name',
   data () {
     return {
       
     }
+  },
+  components: {
+    emailCenter,
   }
 }
 </script>
