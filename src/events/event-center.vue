1c3ff152818a5bc285307286cd1d5128de8e4ec2
pitzbro <asioren@gmail.com>
2016-12-21 18:29:04 +0200

    starting events

--- src/events/event-center.vue
+++ src/events/event-center.vue
@@ -1,59 +1,32 @@
 <template>
   <section>
 
-      <h1></h1>
+      <h1>My Events</h1>
 
   </section>
 </template>
 
 <script>
+import EmailList from './event-list.vue';
+import { data } from '../eventData';
 
-import EmailDetails from './email/email-details.vue';
-import EmailList from './email/email-list.vue';
-import EmailStatus from './email/email-status.vue';
+console.log(data);
 
 export default {
   name: 'app',
   data () {
     return {
-      emails: [
-        {id: 1, subject: 'mail1', body: 'uuheh vtehethns.kdjvnldvl/in ldvinoidnv lndvn', isRead: false},
-        {id: 2, subject: 'mail2', body: 'heheht vns.kdjvnthehheethethoidnv lndvn', isRead: false},
-        {id: 3, subject: 'mail3', body: '88jjjrjvns.ehttehjvnldvl/inehtethnoidnv lndvn', isRead: false},
-        {id: 4, subject: 'mail4', body: 'aqhhjrjjvns.kdjvnehehvl/in ldvinoidnv lndvn', isRead: false},
-        {id: 5, subject: 'mail5', body: 'jjj ns.khteethldvl/in ldvinoidnv lndvn', isRead: false},
-        {id: 6, subject: 'mail6', body: 'rjyrjj vns.ktehl/in ldvinoidnv lnehtdvn', isRead: false}
-      ],
-      seletedEmail: null,
-      emailPicked: true
+      events: []
     }
   },
   computed: {
-    emailsNum: function() {
-      return this.emailsNum = this.emails.length;
-    },
-    unreadEmailsNum: function() {
-      return this.emails.filter(email => email.isRead).length;
-    }
+    
   },
   methods: {
-    selectedEmailId(selectedEmailId) {
-      this.seletedEmail = this.emails.filter(email => selectedEmailId === email.id)[0];
-      this.seletedEmail.isRead = true;
-      if (this.seletedEmail.id) this.emailPicked = true;
-    }, 
-    deletedEmailID(deletedEmailID) {
-      this.emailPicked = false;
-      this.emails = this.emails.filter(email => deletedEmailID !== email.id);
-    }
+    
   },
   components: {
-    EmailDetails,
-    EmailList,
-    EmailStatus
-  },
-  created() {
-    this.seletedEmail = this.emails[0];
+    EventsList
   }
 }
 </script>
