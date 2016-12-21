0058fa367c31750ef4c166baf64d1870b87dcfbb
pitzbro <asioren@gmail.com>
2016-12-21 20:02:23 +0200

    got events

--- src/events/event-center.vue
+++ src/events/event-center.vue
@@ -1,22 +1,20 @@
 <template>
   <section>
 
-      <h1>My Events</h1>
+      <event-list :events="events"></event-list>
 
   </section>
 </template>
 
 <script>
-import EmailList from './event-list.vue';
-import { data } from '../eventData';
-
-console.log(data);
+import EventList from './event-list.vue';
+import eventsData from '../eventsData';
 
 export default {
   name: 'app',
   data () {
     return {
-      events: []
+      events: eventsData.events
     }
   },
   computed: {
@@ -26,38 +24,11 @@ export default {
     
   },
   components: {
-    EventsList
+    EventList
   }
 }
 </script>
 
-
 <style>
-  .email__wrapper {
-    max-width: 960px;
-    margin: 0 auto;
-    border: solid 1px #999;
-  }
-  
-  .email__container {
-    display: flex;
-  }
-  
-  .email__list {
-    width: 150px;
-    background: pink;
-  }
-  
-  .email__details {
-    width: calc(100% - 150px);
-    background: red;
-  }
-  
-  * {
-    box-sizing: border-box;
-  }
 
-  button {
-    cursor: pointer;
-  }
 </style>
---
+++ src/events/event-preview.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="event">
+        <time datetime="2012-12-08">{{event.time}}</time>
+        <h2 class="event__title"> {{event.name}} </h2>
+        <div class="event__description" v-html="event.description">
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    event: {
+      required: true,
+      type: Object
+    }
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
